e20835a71e35283491e2d440ae0802ee53fbde17
princeemeka965 <anyanwue4@gmail.com>
2024-01-06 23:20:36 +0100

    set up image protocol

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'i.dummyjson.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'cdn.dummyjson.com',       
+            }
         ]
     }
 }
